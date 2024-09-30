9bb88d939f6ce2bfb432df85aa4abcee3bf9f20f
vellt <szantobenjamin97@gmail.com>
2024-09-30 18:28:43 +0200

    mai ora

---
+++ WpfApp8/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp8
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            gomb.Click += Gomb_Click;
+        }
+
+        private void Gomb_Click(object sender, RoutedEventArgs e)
+        {
+            if (ker.Text.Trim().Length==0)
+            {
+                MessageBox.Show("A csillaggal jelölt mezőket kötelező kit.");
+            }
+            else
+            {
+                MessageBox.Show($"{vez.Text} {ker.Text}", $"A nevem {ker.Text}");
+            }
+        }
+    }
+}
