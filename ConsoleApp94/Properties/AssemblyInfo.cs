007509075591a1383c4b05bc7ea877b521617baa
vellt <szantobenjamin97@gmail.com>
2025-01-06 18:15:37 +0100

    mai ora

--- ConsoleApp94/Properties/AssemblyInfo.cs
+++ ConsoleApp94/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Storage2")]
+[assembly: AssemblyTitle("ConsoleApp94")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Storage2")]
+[assembly: AssemblyProduct("ConsoleApp94")]
 [assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("63d7da39-6d5d-434e-aa59-03c42301ff30")]
+[assembly: Guid("308136ab-6697-49dc-8166-5a51de58b465")]
 
 // Version information for an assembly consists of the following four values:
 //
